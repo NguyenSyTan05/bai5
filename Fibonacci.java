12fceeb876ab8a721254b8013ba945fc0a810393
NguyenSyTan05 <sytanzxc2005@gmail.com>
2024-11-07 04:14:24 +0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -16,16 +16,13 @@ public class Fibonacci {
         System.out.print("Nhap so phan tu cua day Fibonacci: ");
         int n = scanner.nextInt();
 
-        // Kiểm tra nếu n nhỏ hơn hoặc bằng 0 thì không có phần tử nào để in
         if (n <= 0) {
             System.out.println("Vui long nhap mot so nguyen duong.");
         } else {
             System.out.print("Day Fibonacci voi " + n + " phan tu: ");
 
-            // Khai bao hai so dau tien trong day Fibonacci
             int a = 0, b = 1;
 
-            // Su dung vong lap for de in ra n phan tu dau tien
             for (int i = 0; i < n; i++) {
                 System.out.print(a + " "); 
 
